11d952042b1977890c29e492d754a40e658a6137
DimaBlokhin <vanilaaabear@gmail.com>
2021-11-01 21:22:18 +0700

    Add Tests for DepositService

--- deposit-service/src/main/java/com/javastart/deposit/rest/AccountResponseDTO.java
+++ deposit-service/src/main/java/com/javastart/deposit/rest/AccountResponseDTO.java
@@ -3,12 +3,14 @@ package com.javastart.deposit.rest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountResponseDTO {
